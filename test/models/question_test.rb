16b5e6103725cb4d8e43f2173e4c411347aedd0a
Angus Fletcher <angusiguess@gmail.com>
2013-10-02 20:54:52 -0300

    Add tests to questions

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,6 +1,34 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
+  test "questions should not save without a title" do
+    q = Question.new
+    assert !q.save
+  end
+
+  test "questions should not save without a body" do
+    q = Question.new
+    title = "A title"
+    assert !q.save
+  end
+
+  test "questions should initialize with a votes count of 0" do
+    q = Question.new
+    assert_equal 0, q.votes
+  end
+
+  test "vote_up! should increment the votes count of a question" do
+    q = Question.new
+    q.vote_up!
+    assert_equal 1, q.votes
+  end
+
+  test "vote_down! should decrement the votes count of a question" do
+    q = Question.new
+    q.vote_down!
+    assert_equal -1, q.votes
+  end
+
   # test "the truth" do
   #   assert true
   # end
