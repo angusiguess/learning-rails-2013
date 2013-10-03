07b04f47ea2c9e72d63d33c484b389219cd18d26
Angus Fletcher <angusiguess@gmail.com>
2013-10-02 21:03:17 -0300

    Add factory girl for fixtures

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,10 @@ Bundler.require(:default, Rails.env)
 module LearningRails2013
   class Application < Rails::Application
 
+    config.generators do |g|
+      g.fixture_replacement :factory_girl
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
