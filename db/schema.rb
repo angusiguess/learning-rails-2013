d1149762c803384b7ba041a764f95166a325d214
Angus Fletcher <angusiguess@gmail.com>
2013-10-08 23:49:23 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131002233953) do
+ActiveRecord::Schema.define(version: 20131003233041) do
 
   create_table "questions", force: true do |t|
     t.text     "title"
     t.text     "body"
     t.integer  "votes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "test_models", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
