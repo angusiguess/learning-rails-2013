3ced9b2e95b5a55e6598393650ea052feeb85ee3
Angus Fletcher <angusiguess@gmail.com>
2013-10-08 23:52:22 -0300

    Add user model

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :username, :email, :password, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 LearningRails2013::Application.routes.draw do
+  resources :users
+
   resources :questions
 
   # The priority is based upon order of creation: first created -> highest priority.
